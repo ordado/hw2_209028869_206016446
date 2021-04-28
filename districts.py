1fbe9f5d780f621588b640eb7418a0eeab5ac270
ordad <or.dado@campus.technion.ac.il>
2021-04-28 18:27:15 +0300

    need to continue "print deatiels"

--- districts.py
+++ districts.py
@@ -1,4 +1,5 @@
 import data
+import statistics
 
 
 class Districts:
@@ -7,15 +8,21 @@ class Districts:
 
     def filter_districts(self, letters):
         list_districts = self.dataset.get_all_districts
-        list_index_need_delete=[]
+        list_index_need_delete = []
         for index, run_list in enumerate(list_districts):
-            flag=0
+            flag = 0
             for run_letters in letters:
-                if run_list[0]==run_letters:
-                    flag=1
-            if flag==0:
+                if run_list[0] == run_letters:
+                    flag = 1
+            if flag == 0:
                 list_index_need_delete.append(index)
 
         for index in reversed(list_index_need_delete):
             for key in self.dataset.keys():
                 self.dataset[key].pop(index)
+
+
+    def print_details(self, features, statistic_functions):
+        for run_features in features:
+            for run_statistic in statistic_functions:
+                print()
